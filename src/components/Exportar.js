536075e5aaa6cbc223240ef3e19284f47cdfc2ae
jafajardo2001 <136715245+jafajardo2001@users.noreply.github.com>
2024-08-27 23:17:13 -0500

    Correcion menor

---
+++ src/components/Exportar.js
@@ -0,0 +1,3 @@
+import React, { useState, useEffect } from "react";
+import { Button, Card, Row, Col, Space, Table, Typography, Menu, Dropdown, Spin } from "antd";
+import { SyncOutlined, FileAddOutlined, EditOutlined, DeleteOutlined, MenuOutlined } from "@ant-design/icons";
