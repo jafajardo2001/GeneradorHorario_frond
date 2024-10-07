eee2a427c3858782fe03637fb2b118da20b67be9
jafajardo2001 <136715245+jafajardo2001@users.noreply.github.com>
2024-10-07 15:12:28 -0500

    separacion de perfiles al crear

--- src/components/NewUsuario.js
+++ src/components/NewUsuario.js
@@ -8,9 +8,8 @@ const NewUsuario = (props) => {
   const [isTituloAcademico, setIsTituloAcademico] = useState([]);
   const [isJob, setIsJob] = useState([]);
   const [isCarreras, setIsCarreras] = useState([]);
-
-  // Estado para almacenar múltiples carreras seleccionadas
-  const [selectedCarreras, setSelectedCarreras] = useState([]);
+  const [selectedPerfil, setSelectedPerfil] = useState(null); // Estado para el perfil seleccionado
+  const [selectedCarreras, setSelectedCarreras] = useState([]); // Estado para almacenar las carreras seleccionadas
 
   const Formulario = useRef(null);
   const url = "http://localhost:8000/api/istg/";
@@ -24,99 +23,105 @@ const NewUsuario = (props) => {
   }, [props.isOpen]);
 
   function getRol() {
-    fetch(`${url}show_roles`, { method: 'GET' })
+    fetch(`${url}show_roles`, { method: "GET" })
       .then((response) => response.json())
       .then((data) => {
         let rol = data.data.map((value) => ({
           label: value.descripcion,
-          value: value.id_rol
+          value: value.id_rol,
         }));
         setIsRol(rol);
       })
       .catch((error) => {
-        console.error('Error fetching roles:', error);
+        console.error("Error fetching roles:", error);
       });
   }
 
   function getJobs() {
-    fetch(`${url}show_jobs/`, { method: 'GET' })
+    fetch(`${url}show_jobs/`, { method: "GET" })
       .then((response) => response.json())
       .then((data) => {
         let horas = data.data.map((value) => ({
           label: value.descripcion,
-          value: value.id_job
+          value: value.id_job,
         }));
         setIsJob(horas);
       })
       .catch((error) => {
-        console.error('Error fetching jobs:', error);
+        console.error("Error fetching jobs:", error);
       });
   }
 
   function getTituloAcademico() {
-    fetch(`${url}show_data_titulo_academico`, { method: 'GET' })
+    fetch(`${url}show_data_titulo_academico`, { method: "GET" })
       .then((response) => response.json())
       .then((data) => {
         let titulos = data.data.map((value) => ({
           label: value.descripcion,
-          value: value.id_titulo_academico
+          value: value.id_titulo_academico,
         }));
         setIsTituloAcademico(titulos);
       })
       .catch((error) => {
-        console.error('Error fetching academic titles:', error);
+        console.error("Error fetching academic titles:", error);
       });
   }
 
   function getCarreras() {
-    fetch(`${url}show_carrera`, { method: 'GET' })
+    fetch(`${url}show_carrera`, { method: "GET" })
       .then((response) => response.json())
       .then((data) => {
         let carreras = data.data.map((value) => ({
           label: value.nombre + " (" + value.descripcion_jornada + ")",
-          value: value.id_carrera, // id de la carrera
-          id_jornada: value.id_jornada // id de la jornada
+          value: value.id_carrera,
+          id_jornada: value.id_jornada,
         }));
         setIsCarreras(carreras);
       })
       .catch((error) => {
-        console.error('Error fetching careers:', error);
+        console.error("Error fetching careers:", error);
       });
   }
 
-  // Función para manejar la selección y eliminación de carreras
   function handleCarreraChange(selectedOptions) {
-    setSelectedCarreras(selectedOptions || []);  // Actualiza el estado con las carreras seleccionadas
+    setSelectedCarreras(selectedOptions || []);
   }
 
-  // Función para crear el usuario
+  function handlePerfilChange(selectedOption) {
+    setSelectedPerfil(selectedOption); // Actualizar el perfil seleccionado
+  }
+
   function createUser(value) {
+    const requestBody = {
+      cedula: value.cedula,
+      nombres: value.nombres,
+      apellidos: value.apellidos,
+      correo: value.correo,
+      telefono: value.telefono,
+      id_rol: value.perfil.value,
+      id_titulo_academico: value.tituloAcademico.value,
+      id_job: value.job.value, // Incluye el id_job siempre
+    };
+
+    // Solo añade carreras_jornadas si el perfil seleccionado es "Docente"
+    if (selectedPerfil && selectedPerfil.label === "Docente") {
+      requestBody.carreras_jornadas = selectedCarreras.map((carrera) => ({
+        id_carrera: carrera.value,
+        id_jornada: carrera.id_jornada,
+      }));
+    }
+
     fetch(`${url}create_usuario`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        cedula: value.cedula,
-        nombres: value.nombres,
-        apellidos: value.apellidos,
-        correo: value.correo,
-        telefono: value.telefono,
-        id_rol: value.perfil.value,
-        id_titulo_academico: value.tituloAcademico.value,
-        id_job: value.job.value,
-        // Mapeamos las carreras seleccionadas para enviar tanto id_carrera como id_jornada
-        carreras_jornadas: selectedCarreras.map(carrera => ({
-          id_carrera: carrera.value, // id de la carrera
-          id_jornada: carrera.id_jornada // id de la jornada
-        }))
-      }),
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(requestBody),
     })
-    
       .then((response) => response.json())
       .then((data) => {
         if (data.ok) {
           notification.success({
-            message: 'Éxito',
-            description: data.message || 'Usuario creado con éxito.',
+            message: "Éxito",
+            description: data.message || "Usuario creado con éxito.",
           });
           Formulario.current.resetFields();
           props.onCloseModal();
@@ -124,19 +129,20 @@ const NewUsuario = (props) => {
           setSelectedCarreras([]);  // Resetear carreras seleccionadas
         } else {
           notification.error({
-            message: 'Error',
-            description: data.message || 'Hubo un problema al crear el usuario.',
+            message: "Error",
+            description: data.message || "Hubo un problema al crear el usuario.",
           });
         }
       })
       .catch((error) => {
-        console.error('An error occurred:', error);
+        console.error("An error occurred:", error);
         notification.error({
-          message: 'Error',
-          description: 'Error interno en el servidor',
+          message: "Error",
+          description: "Error interno en el servidor",
         });
       });
   }
+
 
   return (
     <Modal
@@ -210,8 +216,14 @@ const NewUsuario = (props) => {
           </Col>
 
           <Col span={24}>
+            <Form.Item label="Escoja el título académico" name="tituloAcademico">
+              <Select options={isTituloAcademico} />
+            </Form.Item>
+          </Col>
+
+          <Col span={24}>
             <Form.Item label="Escoja el perfil" name="perfil">
-              <Select options={isRol} />
+              <Select options={isRol} onChange={handlePerfilChange} />
             </Form.Item>
           </Col>
 
@@ -221,22 +233,23 @@ const NewUsuario = (props) => {
             </Form.Item>
           </Col>
 
-          <Col span={24}>
-            <Form.Item label="Escoja el título académico" name="tituloAcademico">
-              <Select options={isTituloAcademico} />
-            </Form.Item>
-          </Col>
+          {/* Mostrar las horas solo si el perfil seleccionado es Docente */}
+          {selectedPerfil && selectedPerfil.label === "Docente" && (
+            <>
 
-          <Col span={24}>
-            <Form.Item label="Escoja las carreras" name="carreras">
-              <Select
-                options={isCarreras}
-                isMulti  // Permite la selección múltiple
-                value={selectedCarreras}
-                onChange={handleCarreraChange}  // Maneja cambios en las selecciones
-              />
-            </Form.Item>
-          </Col>
+
+              <Col span={24}>
+                <Form.Item label="Escoja las carreras" name="carreras">
+                  <Select
+                    options={isCarreras}
+                    isMulti
+                    value={selectedCarreras}
+                    onChange={handleCarreraChange}
+                  />
+                </Form.Item>
+              </Col>
+            </>
+          )}
         </Row>
       </Form>
     </Modal>
